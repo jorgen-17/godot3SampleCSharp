33b1f7e85c53c873547446b15ab2210cfb8aed72
typedefJorge <jorgenonelljr@gmail.com>
2018-02-13 23:00:30 -0500

    transfered over gdscript code to move camera and handle animations and motion

--- scripts/Cutesy.cs
+++ scripts/Cutesy.cs
@@ -6,41 +6,57 @@ public class Cutesy : Spatial
     private const string MOVE_FORWARD = "move_forward";
     private const string MOVE_BACK = "move_back";
     private const string RUN_FORWARD = "runForward";
+    private const string ESCAPE = "escape";
     private const int RUNNING_DISPLACEMENT_PER_SECOND = 50;
     private const int ANIMATION_PLAYER_INDEX = 1;
     private const float RUN_ANIMATION_SPEED = 2F;
-    private Vector3 _forward = new Vector3(0, 0, 1);
+    private readonly Vector3 _forward = new Vector3(0, 0, 1);
     private AnimationPlayer _animPlayer;
 
     public override void _Ready()
     {
-        this._animPlayer = this.GetChild(ANIMATION_PLAYER_INDEX) as AnimationPlayer;
-        this._animPlayer.SetCurrentAnimation(RUN_FORWARD);
-        GD.Print(this._animPlayer.GetAutoplay());
-        this._animPlayer.SetSpeedScale(RUN_ANIMATION_SPEED);
+        _animPlayer = GetChild(ANIMATION_PLAYER_INDEX) as AnimationPlayer;
+        _animPlayer.SetCurrentAnimation(RUN_FORWARD);
+        _animPlayer.GetAnimation(RUN_FORWARD).Loop = true;
+        _animPlayer.Stop();
     }
 
     public override void _Process(float delta)
-    {
-        if(Input.IsActionPressed(MOVE_FORWARD))
+    {   
+        if(Input.IsActionJustPressed(ESCAPE))
+        {
+            GetTree().Quit();
+        }
+
+        if(Input.IsActionJustPressed(MOVE_FORWARD))
+        {
+            _animPlayer.Play(RUN_FORWARD, -1, RUN_ANIMATION_SPEED);
+        }
+        else if(Input.IsActionPressed(MOVE_FORWARD))
         {
             var displacement = RUNNING_DISPLACEMENT_PER_SECOND * delta;
-            this.Translate(displacement * this._forward);
-            this._animPlayer.Advance(delta);
+            Translate(displacement * _forward);
+            _animPlayer.Advance(delta);
         }
         else if(Input.IsActionJustReleased(MOVE_FORWARD))
         {
-            this._animPlayer.Seek(0, true);
+            _animPlayer.Stop();
+            _animPlayer.Seek(0, true);
         }
-        if(Input.IsActionPressed(MOVE_BACK))
+        if(Input.IsActionJustPressed(MOVE_BACK))
+        {
+            _animPlayer.PlayBackwards(RUN_FORWARD);
+        }
+        else if(Input.IsActionPressed(MOVE_BACK))
         {
             var displacement = -RUNNING_DISPLACEMENT_PER_SECOND * delta;
-            this.Translate(displacement * this._forward);
-            this._animPlayer.Advance(-delta);
+            Translate(displacement * _forward);
+            _animPlayer.Advance(delta);
         }
         else if(Input.IsActionJustReleased(MOVE_BACK))
         {
-            this._animPlayer.Seek(0, true);
+            _animPlayer.Stop();
+            _animPlayer.Seek(0, true);
         }
     }
 }
--- scripts/ThirdPersonCamera.cs
+++ scripts/ThirdPersonCamera.cs
@@ -9,9 +9,54 @@ public class ThirdPersonCamera : Camera
 
     public override void _Ready()
     {
+        _cutesy = GetTree().GetRoot().GetNode("Node/Cutesy") as Spatial;
         Input.SetMouseMode(Input.MouseMode.Captured);
     }
 
+    public override void _Input(InputEvent @event)
+    {
+        if(@event is InputEventMouseMotion mouseMotionEvent)
+        {
+            ProcessMouseInputEvent(mouseMotionEvent);
+        }
+    }
+
+    private void ProcessMouseInputEvent(InputEventMouseMotion @mouseMotionEvent)
+    {
+        var cutesyPos = _cutesy.GetTranslation();
+        var currentPos = cutesyPos + GetTranslation();
+	
+//         	GD.Print("cutesy at:");
+//         	GD.Print(cutesyPos);
+//         	GD.Print("camera at:");
+//         	GD.Print(currentPos);
+
+        var xAngle = @mouseMotionEvent.Relative.x;
+//         	GD.Print("rotating by:");
+//         	GD.Print(xAngle);
+        var horizontalCenter = new Vector2(cutesyPos.x, cutesyPos.z);
+        var horizontalPoint = new Vector2(currentPos.x, currentPos.z);
+        var rotatedHorizontal = RotateAroundCenter(horizontalPoint, horizontalCenter, xAngle);
+        var horizontalCoordinate = new Vector3(rotatedHorizontal.x, currentPos.y, rotatedHorizontal.y);
+//         	GD.Print("camera getting translated to:");
+//         	GD.Print(horizontalCoordinate);
+	
+        SetTranslation(horizontalCoordinate - cutesyPos);
+	
+//         	GD.Print("\n");
+
+        currentPos = cutesyPos + GetTranslation();
+        var yAngle = @mouseMotionEvent.Relative.y;
+        var verticalCenter = new Vector2(cutesyPos.y, cutesyPos.z);
+        var verticalPoint = new Vector2(currentPos.y, currentPos.z);
+        var rotatedVertical = RotateAroundCenter(verticalPoint, verticalCenter, yAngle);
+        var verticalCoordinate = new Vector3(currentPos.x, rotatedVertical.x, rotatedVertical.y);
+
+        SetTranslation(verticalCoordinate - cutesyPos);
+        LookAt(cutesyPos, new Vector3(0, 1, 0));
+
+    }
+    
     public override void _Process(float delta) { }
 
     public static Vector2 RotateAroundCenter(Vector2 point, Vector2 center, float degrees)
@@ -21,7 +66,7 @@ public class ThirdPersonCamera : Camera
         var rotatedY = (float) (Math.Sin(angle) * (point.x - center.x) + Math.Cos(angle) * (point.y - center.y) + center.y);
         return new Vector2(rotatedX, rotatedY);
     }
-    
+
     private static float DegreeToRadian(float angle)
     {
         return (float) ((Math.PI * angle) / 180.0);
